bbb23699bbb1306ad4022f3dc721a9bef5fa2dfc
Natalie-Cardot <nataliecardot@gmail.com>
2018-10-24 18:10:27 -0700

    Updates

--- precache-manifest.712402f635a2c17c25bfdce8dd1259a0.js
+++ precache-manifest.712402f635a2c17c25bfdce8dd1259a0.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/react-neighborhood-map/static/js/runtime~main.df449222.js"
   },
   {
-    "revision": "d53996d37310e7d0fde3",
-    "url": "/react-neighborhood-map/static/js/main.d53996d3.chunk.js"
+    "revision": "2a1f91824c7778eecf8c",
+    "url": "/react-neighborhood-map/static/js/main.2a1f9182.chunk.js"
   },
   {
     "revision": "1fec9b311aae5de51ca0",
     "url": "/react-neighborhood-map/static/js/1.1fec9b31.chunk.js"
   },
   {
-    "revision": "d53996d37310e7d0fde3",
-    "url": "/react-neighborhood-map/static/css/main.07fad6a4.chunk.css"
+    "revision": "2a1f91824c7778eecf8c",
+    "url": "/react-neighborhood-map/static/css/main.44c8ab8a.chunk.css"
   },
   {
-    "revision": "530f84ee593567d73d1a0ff3811edbec",
+    "revision": "a5878f8eb0cdd5522d20d79936805760",
     "url": "/react-neighborhood-map/index.html"
   }
 ];
