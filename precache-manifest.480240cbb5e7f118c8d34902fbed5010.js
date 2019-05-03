e1a40aa1a7684737f9f577e8be766a73cda98a21
Natalie-Cardot <nataliecardot@gmail.com>
2019-05-02 20:37:26 -0700

    Updates

---
+++ precache-manifest.480240cbb5e7f118c8d34902fbed5010.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4de67e9c09592e47c025",
+    "url": "/react-neighborhood-map/static/css/main.4e7385f0.chunk.css"
+  },
+  {
+    "revision": "4de67e9c09592e47c025",
+    "url": "/react-neighborhood-map/static/js/main.4de67e9c.chunk.js"
+  },
+  {
+    "revision": "333074240657e4c0b0c7",
+    "url": "/react-neighborhood-map/static/js/1.33307424.chunk.js"
+  },
+  {
+    "revision": "df449222aabb8d7a6ef7",
+    "url": "/react-neighborhood-map/static/js/runtime~main.df449222.js"
+  },
+  {
+    "revision": "079a8f01cd49e73874e7c4d17aabcdfa",
+    "url": "/react-neighborhood-map/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/react-neighborhood-map/precache-manifest.9b42b0e0510fb463217ab2aaf248a59f.js"
+  "/react-neighborhood-map/precache-manifest.480240cbb5e7f118c8d34902fbed5010.js"
 );
 
 workbox.clientsClaim();
