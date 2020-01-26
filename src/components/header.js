7ad51671b535a1ed64d0da51ae0894ac2010df3e
Natalie-Cardot <nataliecardot@gmail.com>
2020-01-26 00:10:45 -0800

    Adjust styling, change favicon

--- src/components/header.js
+++ src/components/header.js
@@ -3,7 +3,9 @@ import React, { Component } from 'react';
 export default class Header extends Component {
   render() {
     return (
-      <h1 className="header" role="banner" tabIndex="1">Seattle Scoops</h1>
+      <h1 className="header" role="banner" tabIndex="1">
+        Seattle Scoops
+      </h1>
     );
   }
 }
--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -18,58 +18,58 @@ export default class Sidebar extends Component {
   handleVenueSearch = () => {
     // If search with whitespace removed is not blank, sets venues (accessed from spreading of state in this component's rendering in App.js) to new array of venues filtered by venues with names that include search input, and returns that new venues array
     if (this.state.query.trim() !== '') {
-      const venues = this.props.venues.filter(venue => venue.name
-        .toLowerCase()
-        .includes(this.state.query.toLowerCase()))
+      const venues = this.props.venues.filter(venue =>
+        venue.name.toLowerCase().includes(this.state.query.toLowerCase())
+      );
       return venues;
     }
     // Return this if the query is a blank string
     return this.props.venues;
-  }
+  };
 
   handleChange = e => {
     // Using object setState (rather than function setState) because new state does not depend on previous state
-    this.setState({query: e.target.value});
+    this.setState({ query: e.target.value });
     // For hiding markers after something has been entered into search box. For each item in venues array (from spreading this.state in Sidebar component rendering in app.js), sets isMatched to search input that is at least part of the name of venue (both lowercase)
     const markers = this.props.venues.map(venue => {
       const isMatched = venue.name
-      .toLowerCase()
-      .includes(e.target.value.toLowerCase());
+        .toLowerCase()
+        .includes(e.target.value.toLowerCase());
       // find() returns value of first element in array that satisfies provided testing function. Otherwise undefined is returned
       const marker = this.props.markers.find(marker => marker.id === venue.id);
       // If search input matches at least partially a venue name, sets corresponding marker to visible, and hides markers for nonmatching venues
-      isMatched ? marker.isVisible = true : marker.isVisible = false;
+      isMatched ? (marker.isVisible = true) : (marker.isVisible = false);
       return marker;
     });
     // Using ES6 property shorthand. Method is in app.js. Only mutating isVisible
-    this.props.updateMarkerState({markers});
-  }
+    this.props.updateMarkerState({ markers });
+  };
 
   render() {
     return (
       <>
         <MediaQuery query="(max-device-width: 767px)">
-        <Menu>
-          <div className="sidebar">
-            <Header />
-            <input
-              tabIndex="2"
-              type="search"
-              className="search"
-              placeholder="Search"
-              onChange={this.handleChange}
-              role="search"
-              aria-label="search"
-            />
-            <VenuesList
-              // Making this component's props available in VenuesList
-              {...this.props}
-              venues={this.handleVenueSearch()}
-              // Method is in app.js
-              handleVenuesListItemClick={this.props.handleVenuesListItemClick}
-            />
-          </div>
-        </Menu>
+          <Menu>
+            <div className="sidebar">
+              <Header />
+              <input
+                tabIndex="2"
+                type="search"
+                className="search"
+                placeholder="Search..."
+                onChange={this.handleChange}
+                role="search"
+                aria-label="search"
+              />
+              <VenuesList
+                // Making this component's props available in VenuesList
+                {...this.props}
+                venues={this.handleVenueSearch()}
+                // Method is in app.js
+                handleVenuesListItemClick={this.props.handleVenuesListItemClick}
+              />
+            </div>
+          </Menu>
         </MediaQuery>
         <MediaQuery query="(min-device-width: 768px)">
           <div className="sidebar">
@@ -78,7 +78,7 @@ export default class Sidebar extends Component {
               tabIndex="2"
               type="search"
               className="search"
-              placeholder="Search"
+              placeholder="Search..."
               onChange={this.handleChange}
               role="search"
               aria-label="search"
